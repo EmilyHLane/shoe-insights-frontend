90010a76ec9ca6a55b5079059f054ea656c3bf75
EMILYHLANE <embem80@gmail.com>
2019-02-10 19:12:31 -0800

    updates foot shape image sizes, adds test for adding foot shape to database

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
 // alert("js is working");
+
+//////////////////////////
+// find shoes
+//////////////////////////
+
+// 1. capture selections
+// selection variables
+let footShape = null;
+let gender = "";
+let category = "";
+let subCategory = "";
+let womensSize = null;
+let womensWidth = "";
+let brand = "";
+let price = "";
+let color = "";
+let bootShaft = "";
+let heelHeight = "";
+let heelStyle = "";
+let toeStyle = "";
+let occasion = "";
+let materials = "";
+let features = "";
+let performance = "";
+let theme = "";
+
+// get foot type value
+function getFootShape() {
+  footShape = $(this).data("foot-shape-id");
+}
+$(".foot-button").click(getFootShape);
+
+// 2. send to database
+
+const baseURL = "https://shoe-insights-backend.herokuapp.com/api/shoe";
+
+function submitSearch() {
+  console.log("ok");
+  console.log(footShape);
+  $.post(baseURL, {
+    footShape
+    //   gender,
+    //   category,
+    //   subCategory,
+    //   womensSize,
+    //   womensWidth,
+    //   brand,
+    //   price,
+    //   color,
+    //   bootShaft,
+    //   heelHeight,
+    //   heelStyle,
+    //   toeStyle,
+    //   occasion,
+    //   materials,
+    //   features,
+    //   performance,
+    //   theme
+  }).done(console.log("added to db"));
+}
+
+$("#search").click(submitSearch);
