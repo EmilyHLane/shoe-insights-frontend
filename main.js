62a4a7e0e16ccf6e0c40cf302b91bc502ae1075a
EMILYHLANE <embem80@gmail.com>
2019-02-10 22:15:11 -0800

    adds question 2 placeholder on click

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // alert("js is working");
 
 //////////////////////////
-// find shoes
+//      find shoes
 //////////////////////////
 
+//////////////////////////
 // 1. capture selections
+
 // selection variables
 let footShape = null;
 let gender = "";
-let category = "";
-let subCategory = "";
 let womensSize = null;
 let womensWidth = "";
+let category = "";
+let subCategory = "";
 let brand = "";
 let price = "";
 let color = "";
@@ -31,7 +33,47 @@ function getFootShape() {
 }
 $(".foot-button").click(getFootShape);
 
-// 2. send to database
+///////////////////////////////////////////////////////////////////
+// 2. when user selects next Q, hide current div and show next div
+
+// error msg for required questions
+function stepError() {
+  //error message display
+  console.log("error msg: please make a selection");
+}
+
+// Q1 required
+function goQ2() {
+  if (footShape != null) {
+    // hide step 1, show step 2 q1-foot-shape, q2-shoe-size
+    $(".q1-foot-shape").removeClass("flex-column-override");
+    $(".q1-foot-shape").addClass("hide");
+    $(".q2-shoe-size").removeClass("hide");
+    $(".q2-shoe-size").addClass("flex-column-override");
+  } else {
+    stepError();
+  }
+}
+
+$(".btn-step-2").click(goQ2);
+
+// Q2 required
+function goQ3() {
+  if (footShape != null) {
+    // hide step 1, show step 2 q1-foot-shape, q2-shoe-size
+    $(".q2-foot-shape").removeClass("flex-column-override");
+    $(".q2-foot-shape").addClass("hide");
+    $(".q3-shoe-size").removeClass("hide");
+    $(".q3-shoe-size").addClass("flex-column-override");
+  } else {
+    stepError();
+  }
+}
+
+$(".btn-step-3").click(goQ3);
+
+//////////////////////////////////
+// 3. when done, send to database
 
 const baseURL = "https://shoe-insights-backend.herokuapp.com/api/shoe";
 
@@ -41,10 +83,10 @@ function submitSearch() {
   $.post(baseURL, {
     footShape
     //   gender,
-    //   category,
-    //   subCategory,
     //   womensSize,
     //   womensWidth,
+    //   category,
+    //   subCategory,
     //   brand,
     //   price,
     //   color,
